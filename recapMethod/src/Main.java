7a1472bcf31ca687a0f80acb15b6c8bd4f9845fa
Onur Nasır <onur.nasr@gmail.com>
2020-08-02 19:41:04 +0300

    RecapMethod Example Added.

---
+++ recapMethod/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Toplama");
+        toplama(5, 6);
+        /*System.out.println("Çıkarma");
+        cikarma(10, 2);
+        cikarma2(8, 12);
+        System.out.println("Çarpma");
+        carpma(5, 6);
+        System.out.println("Bölme");
+        bolme(10, 2);*/
+
+    }
+    public static void toplama(int sayi1, int sayi2) {
+        int sonuc = sayi1 + sayi2;
+        System.out.println(sayi1+"+"+sayi2+"="+sonuc);
+    }
+}
